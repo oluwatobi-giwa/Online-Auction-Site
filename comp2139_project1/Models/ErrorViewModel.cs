f5c33a8f811de6f0262f63aa7ceb1419f47ad304
Toubielawbar <toubielawbar@gmail.com>
2023-03-03 14:03:18 -0500

    Add project files.

---
+++ comp2139_project1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace comp2139_project1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
